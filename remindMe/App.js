a76438d14667d0622aadefd0fecb522c830df700
Arbelaezch <arbelaezch@gmail.com>
2022-10-16 13:21:27 -0600

    Adds modal

--- remindMe/App.js
+++ remindMe/App.js
@@ -8,7 +8,8 @@
 
 import React, {Component} from 'react';
 import type {Node} from 'react';
-import {Routes, Route, useNavigate} from 'react-router-dom'
+// import {Routes, Route, useNavigate} from 'react-router-dom'
+// import {NavigationContainer} from 'react-navigation/native'
 import {
   SafeAreaView,
   ScrollView,
@@ -21,7 +22,9 @@ import {
   Linking,
   TouchableOpacity,
   Button,
-  Alert
+  Alert,
+  Modal,
+  Pressable,
 } from 'react-native';
 
 import {
@@ -34,59 +37,82 @@ import {
 
 
 
-
-
-
-
-
-
-
-
-class ScrollViewExample extends Component {
+export default class App extends Component {
   state = {
-     names: [
-        {'name': 'Ben', 'id': 1},
-        {'name': 'Susan', 'id': 2},
-        {'name': 'Robert', 'id': 3},
-        {'name': 'Mary', 'id': 4},
-        {'name': 'Daniel', 'id': 5},
-        {'name': 'Laura', 'id': 6},
-        {'name': 'John', 'id': 7},
-        {'name': 'Debra', 'id': 8},
-        {'name': 'Aron', 'id': 9},
-        {'name': 'Ann', 'id': 10},
-        {'name': 'Steve', 'id': 11},
-        {'name': 'Olivia', 'id': 12}
-     ]
+    names: [
+       {'name': 'Ben', 'id': 1},
+       {'name': 'Susan', 'id': 2},
+       {'name': 'Robert', 'id': 3},
+       {'name': 'Mary', 'id': 4},
+       {'name': 'Daniel', 'id': 5},
+       {'name': 'Laura', 'id': 6},
+       {'name': 'John', 'id': 7},
+       {'name': 'Debra', 'id': 8},
+       {'name': 'Aron', 'id': 9},
+       {'name': 'Ann', 'id': 10},
+       {'name': 'Steve', 'id': 11},
+       {'name': 'Olivia', 'id': 12}
+    ],
+    modalVisible: false
+  };
+
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
   }
 
   render(props) {
-
-    // const navigate = useNavigate();
-
-    const navigateToReminder = () => {
+    const { modalVisible } = this.state
+    return (
+      <View style={styles.centeredView}>
       
-    }
+      {/* MODAL */}
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          this.setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => this.setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+      </Modal>
 
-     return (
-        <View style={{flex :1, justifyContent: 'center', margin: 15 }}>
-          <ScrollView>
-              {this.state.names.map((item, index) => (
-                <TouchableOpacity onPress={navigateToReminder}>
-                  <View key = {item.id} style={styles.item}>   
-                    <Text style={{ color: 'white'}}>
-                      {item.name}
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              ))}
-          </ScrollView>
-          <Button title='Add item' onPress={addReminder}></Button>
-        </View>
-     );
+
+      {/* LIST */}
+      {<View style={{flex :1, justifyContent: 'center', margin: 15 }}>
+        <ScrollView>
+            {this.state.names.map((item, index) => (
+              <TouchableOpacity onPress={() => this.setModalVisible(true)}>
+                <View key = {item.id} style={styles.item}>   
+                  <Text style={{ color: 'white'}}>
+                    {item.name}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+        </ScrollView>
+        <Button title='Add item' onPress={addReminder}></Button>
+      </View>}
+      
+      
+      
+      </View>
+    );
   }
 }
-export default ScrollViewExample;
+
+
 const styles = StyleSheet.create ({
   item: {
      flexDirection: 'row',
@@ -97,8 +123,55 @@ const styles = StyleSheet.create ({
      borderColor: '#2a4944',
      borderWidth: 1,
      backgroundColor: 'black',
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
-})
+});
+
+
+
+function navigateToReminder(id) {
+  return <h2>Test</h2>
+}
 
 function addReminder() {
   Alert.alert(
@@ -116,76 +189,3 @@ function addReminder() {
 function reminderPage() {
   return <h2>Reminder Page</h2>
 }
-
-
-
-
-
-// const logo = {
-//   uri: 'https://reactnative.dev/img/tiny_logo.png',
-//   width: 64,
-//   height: 64
-// }
-
-// const styles = {
-  
-// }
-// state = {
-//   names: [
-//      {'name': 'Ben', 'id': 1},
-//      {'name': 'Susan', 'id': 2},
-//      {'name': 'Robert', 'id': 3},
-//      {'name': 'Mary', 'id': 4},
-//      {'name': 'Daniel', 'id': 5},
-//      {'name': 'Laura', 'id': 6},
-//      {'name': 'John', 'id': 7},
-//      {'name': 'Debra', 'id': 8},
-//      {'name': 'Aron', 'id': 9},
-//      {'name': 'Ann', 'id': 10},
-//      {'name': 'Steve', 'id': 11},
-//      {'name': 'Olivia', 'id': 12}
-//   ]
-// }
-
-// const App = () => (
-//   <ScrollView>
-//     <Text style={{ fontSize: 22 }}>Scroll me plz</Text>
-
-//     {this.state.names.map((item, index) => (<View key = {item.id} style = {styles.item}><Text>{item.name}</Text></View>))}    <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Text style={{ fontSize: 96 }}>If you like</Text>
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Text style={{ fontSize: 96 }}>Scrolling down</Text>
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Text style={{ fontSize: 96 }}>What's the best</Text>
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Text style={{ fontSize: 96 }}>Framework around?</Text>
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Image source={logo} />
-//     <Text style={{ fontSize: 80 }}>React Native</Text>
-//   </ScrollView>
-// );
-
-
-// export default App;
-
-
-
